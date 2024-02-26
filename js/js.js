41a8651f47ce1771f8648cd6ec98fb5e7acb2eac
slim97397 <130625131+slim97397@users.noreply.github.com>
2024-02-26 09:32:04 -0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,36 @@
+function randomposition(){
+    let face = document.getElementById("face");
+    face.classList.toggle("random");
+
+    let hat = document.getElementById("hat");
+    hat.classList.toggle("random");
+
+    let nose = document.getElementById("nose");
+    nose.classList.toggle("random");
+
+    let eyel = document.getElementById("eyel");
+    eyel.classList.toggle("random");
+
+    let eyer = document.getElementById("eyer");
+    eyer.classList.toggle("random");
+
+    let mos = document.getElementById("mos");
+    mos.classList.toggle("random");
+
+    let earl = document.getElementById("earl");
+    earl.classList.toggle("random");
+
+    let earr = document.getElementById("earr");
+    earr.classList.toggle("random");
+
+    let fotl = document.getElementById("fotl");
+    fotl.classList.toggle("random");
+
+    let fotr = document.getElementById("fotr");
+    fotr.classList.toggle("random");
+
+    let fondo = document.getElementById("fondo");
+    fondo.classList.toggle("random");
+
+    alert("Se va a descontrolar");
+}
